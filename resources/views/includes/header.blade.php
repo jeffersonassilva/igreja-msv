c383fe6c39df038d95d3a13e7ef173aa0457046a
Jefferson Silva <jeffersonassilva@gmail.com>
2022-02-05 23:58:23 -0300

    feat: correção dos links dos menus

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -5,9 +5,9 @@
     <div class="toggle"></div>
     <ul class="navigation">
         <li class="link"><a href="{{ route('index') }}">Home</a></li>
-        <li class="link"><a href="#fotos">Fotos</a></li>
-        <li class="link"><a href="#programacao">Programação</a></li>
-        <li class="link"><a href="#contato">Contato</a></li>
+        <li class="link"><a href="/#fotos">Fotos</a></li>
+        <li class="link"><a href="/#programacao">Programação</a></li>
+        <li class="link"><a href="/#contato">Contato</a></li>
         {{-- <li><a href="{{ route('login') }}">Login</a></li>--}}
     </ul>
 </header>
