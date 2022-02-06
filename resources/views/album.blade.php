609fcc712ff1776ec7b3dd5ca8f5e5a05863e165
Jefferson Silva <jeffersonassilva@gmail.com>
2022-02-06 16:02:54 -0300

    feat: removendo a capa do álbum na exibição das fotos

--- resources/views/album.blade.php
+++ resources/views/album.blade.php
@@ -3,11 +3,13 @@
 @section('content')
     <div class="fotografias">
     @foreach(File::glob(public_path('albuns/' . $album['pasta']).'/*') as $path)
+        @if(!strpos(str_replace(public_path(), '', $path), 'capa'))
         <div class="fotografia">
             <a href="{{ str_replace(public_path(), '', $path) }}" data-fancybox="gallery">
                 <img src="{{ str_replace(public_path(), '', $path) }}">
             </a>
         </div>
+        @endif
     @endforeach
     </div>
 @endsection
