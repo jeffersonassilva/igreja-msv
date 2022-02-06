c8335d5e58a5f6450bea84d6912f142e37e3444e
Jefferson Silva <jeffersonassilva@gmail.com>
2022-02-05 23:21:49 -0300

    feat: adicionando a área de programação da igreja

---
+++ resources/views/includes/programacao.blade.php
@@ -0,0 +1,54 @@
+<section id="programacao" class="programacao">
+    <h1>Programação</h1>
+    <div class="cultos">
+        <div>
+            <ion-icon name="calendar-outline"></ion-icon>
+            <div class="info">
+                <p>Terça-feira</p>
+                <div>
+                    <span>20:00h</span> - Culto de Oração
+                </div>
+            </div>
+        </div>
+        <div>
+            <ion-icon name="calendar-outline"></ion-icon>
+            <div class="info">
+                <p>Quarta-feira</p>
+                <div>
+                    <span>16:00h</span> - Culto de Oração
+                </div>
+            </div>
+        </div>
+        <div>
+            <ion-icon name="calendar-outline"></ion-icon>
+            <div class="info">
+                <p>Quinta-feira</p>
+                <div>
+                    <span>20:00h</span> - Culto de Oração
+                </div>
+            </div>
+        </div>
+        <div>
+            <ion-icon name="calendar-outline"></ion-icon>
+            <div class="info">
+                <p>Sábado</p>
+                <div>
+                    <span>20:00h</span> - Culto dos Jovens
+                </div>
+            </div>
+        </div>
+        <div>
+            <ion-icon name="calendar-outline"></ion-icon>
+            <div class="info">
+                <p>Domingo</p>
+                <div>
+                    <span>09:30h</span> - Escola Bíblica Dominical
+                </div>
+                <div>
+                    <span>19:00h</span> - Culto de Louvor e Adoração a Deus
+                </div>
+            </div>
+        </div>
+        <div></div>
+    </div>
+</section>
