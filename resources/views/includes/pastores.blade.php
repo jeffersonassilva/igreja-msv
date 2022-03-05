7aceb8551019c68d02fa67d9cd60f28c3cae899d
Jefferson Silva <jeffersonassilva@gmail.com>
2022-03-05 01:12:19 -0300

    style: ajustes no layout

--- resources/views/includes/pastores.blade.php
+++ resources/views/includes/pastores.blade.php
@@ -1,16 +1,18 @@
-<section id="pastores" class="pastores">
-    <div>
-        <div>
-            <div class="titulo">Pastores</div>
-            <div class="nomes">SAMUEL e HACSA NOVAIS</div>
-            <div class="descricao">
-                O Ministério Semeando a Verdade foi um projeto que Deus colocou em nossos corações.
-                Cremos que o Senhor nos deu entendimento a respeito dos nossos ministérios, e nos direcionou para realizar
-                aquilo que nos foi confiado.
+<section id="pastores" class="p-3 md:p-0 md:py-10 md:min-h-[600px] bg-neutral-100">
+    <div class="container mx-auto max-w-[1280px]">
+        <h3 class="titulo-separador">Pastores</h3>
+        <div class="flex flex-col flex-wrap md:flex-row p-3 lg:py-10 lg:px-20">
+            <div class="lg:flex-[50%] lg:flex-1 flex md:justify-end lg:px-3">
+                <img class="w-full lg:max-w-[600px]" src="{{ asset('img/pastores.jpg') }}" alt="Samuel_Hacsa" title="Samuel_Hacsa">
             </div>
-        </div>
-        <div>
-            <img src="{{ asset('img/pastores.jpg') }}" alt="Samuel_Hacsa" title="Samuel_Hacsa">
+            <div class="lg:flex-[50%] lg:flex-1">
+                <div class="text-lg lg:text-2xl font-medium my-6 lg:my-0 lg:mx-6 2xl:mx-10 text-neutral-700">SAMUEL e HACSA NOVAIS</div>
+                <div class="font-light  text-sm lg:text-base 2xl:text-xl leading-6 lg:leading-7 2xl:leading-9 mb-6 lg:m-6 2xl:m-10">
+                    O Ministério Semeando a Verdade foi um projeto que Deus colocou em nossos corações.
+                    Cremos que o Senhor nos deu entendimento a respeito dos nossos ministérios, e nos direcionou para realizar
+                    aquilo que nos foi confiado.
+                </div>
+            </div>
         </div>
     </div>
 </section>
--- resources/views/includes/propositos.blade.php
+++ resources/views/includes/propositos.blade.php
@@ -1,26 +1,29 @@
-<section id="propositos" class="propositos">
-    <div class="valores">
-        <div class="item">
-            <ion-icon name="locate-outline"></ion-icon>
-            <h1>Missão</h1>
-            <div>A Igreja Evangélica Ministério Semeando a Verdade tem como missão prestar assistência religiosa,
-                congregar seus membros para adoração a Deus, estudar a Bíblia, expandir o evangelho do nosso Senhor
-                Jesus Cristo.
+<section id="propositos" class="p-3 md:p-0 md:py-10 md:min-h-[550px]">
+    <div class="container mx-auto max-w-[1280px]">
+        <h3 class="titulo-separador">Propósitos</h3>
+        <div class="valores">
+            <div class="item">
+                <ion-icon name="locate-outline"></ion-icon>
+                <h1>Missão</h1>
+                <div>A Igreja Evangélica Ministério Semeando a Verdade tem como missão prestar assistência religiosa,
+                    congregar seus membros para adoração a Deus, estudar a Bíblia, expandir o evangelho do nosso Senhor
+                    Jesus Cristo.
+                </div>
             </div>
-        </div>
-        <div class="item">
-            <ion-icon name="earth"></ion-icon>
-            <h1>Nossa Visão</h1>
-            <div>Ser uma igreja relevante baseada na multiplicação intencional e fundamentada na grande comissão
-                (chamar, acolher e aperfeiçoar). Através de relacionamentos discipuladores ganhar almas para Jesus em
-                nossa cidade e até os confins da terra. Lutar pela unidade da igreja como corpo de Cristo e expandir o
-                reino de Deus nos lares.
+            <div class="item">
+                <ion-icon name="earth"></ion-icon>
+                <h1>Nossa Visão</h1>
+                <div>Ser uma igreja relevante baseada na multiplicação intencional e fundamentada na grande comissão
+                    (chamar, acolher e aperfeiçoar). Através de relacionamentos discipuladores ganhar almas para Jesus em
+                    nossa cidade e até os confins da terra. Lutar pela unidade da igreja como corpo de Cristo e expandir o
+                    reino de Deus nos lares.
+                </div>
             </div>
-        </div>
-        <div class="item">
-            <ion-icon name="heart"></ion-icon>
-            <h1>Propósitos</h1>
-            <div>Texto a definir</div>
+            <div class="item">
+                <ion-icon name="heart"></ion-icon>
+                <h1>Propósitos</h1>
+                <div>Texto a definir</div>
+            </div>
         </div>
     </div>
 </section>
