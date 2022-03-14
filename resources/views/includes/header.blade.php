d9efb60bad5f1cdb5edc075ae58f2630e5480023
Jefferson Silva <jeffersonassilva@gmail.com>
2022-03-14 17:41:07 -0300

    feat: adicionando o menu de ofertas

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -6,6 +6,7 @@
     <ul class="navigation">
         <li class="link"><a href="{{ route('index') }}">Home</a></li>
         <li class="link"><a href="/#propositos">Propósitos</a></li>
+        <li class="link"><a href="{{ route('ofertas') }}">Ofertas</a></li>
         <li class="link"><a href="/#fotos">Fotos</a></li>
         <li class="link"><a href="/#pastores">Pastores</a></li>
         <li class="link"><a href="/#programacao">Programação</a></li>
