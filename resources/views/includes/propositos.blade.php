bdafaa18e7aa34938652ddbcb3f7d7bce479c62f
Jefferson Silva <jeffersonassilva@gmail.com>
2022-03-15 10:57:28 -0300

    feat: adicionando o texto de propósitos

--- resources/views/includes/propositos.blade.php
+++ resources/views/includes/propositos.blade.php
@@ -3,26 +3,38 @@
         <h3 class="titulo-separador">Propósitos</h3>
         <div class="grid p-3 gap-6 lg:grid-cols-3 lg:px-6">
             <div class="flex flex-col items-center w-full bg-gray-50 rounded-lg p-6">
-                <div class="flex-none text-yellow-500 text-2xl sm:text-3xl"><ion-icon name="locate-outline"></ion-icon></div>
+                <div class="flex-none text-yellow-500 text-2xl sm:text-3xl">
+                    <ion-icon name="locate-outline"></ion-icon>
+                </div>
                 <h1 class="pb-6 sm:text-lg">Missão</h1>
-                <div class="font-light text-sm text-center leading-6 sm:text-base">A Igreja Evangélica Ministério Semeando a Verdade tem como missão prestar assistência religiosa,
+                <div class="font-light text-sm text-center sm:text-base">
+                    A Igreja Evangélica Ministério Semeando a Verdade tem como missão prestar assistência religiosa,
                     congregar seus membros para adoração a Deus, estudar a Bíblia, expandir o evangelho do nosso Senhor
                     Jesus Cristo.
                 </div>
             </div>
             <div class="flex flex-col items-center w-full bg-gray-50 rounded-lg p-6">
-                <div class="flex-none text-yellow-500 text-2xl sm:text-3xl"><ion-icon name="earth"></ion-icon></div>
+                <div class="flex-none text-yellow-500 text-2xl sm:text-3xl">
+                    <ion-icon name="earth"></ion-icon>
+                </div>
                 <h1 class="pb-6 sm:text-lg">Nossa Visão</h1>
-                <div class="font-light text-sm text-center leading-6 sm:text-base">Ser uma igreja relevante baseada na multiplicação intencional e fundamentada na grande comissão
-                    (chamar, acolher e aperfeiçoar). Através de relacionamentos discipuladores ganhar almas para Jesus em
-                    nossa cidade e até os confins da terra. Lutar pela unidade da igreja como corpo de Cristo e expandir o
-                    reino de Deus nos lares.
+                <div class="font-light text-sm text-center sm:text-base">
+                    Ser uma igreja relevante baseada na multiplicação intencional e fundamentada na grande comissão
+                    (chamar, acolher e aperfeiçoar). Através de relacionamentos discipuladores ganhar almas para Jesus
+                    em nossa cidade e até os confins da terra. Lutar pela unidade da igreja como corpo de Cristo e
+                    expandir o reino de Deus nos lares.
                 </div>
             </div>
             <div class="flex flex-col items-center w-full bg-gray-50 rounded-lg p-6">
-                <div class="flex-none text-yellow-500 text-2xl sm:text-3xl"><ion-icon name="heart"></ion-icon></div>
+                <div class="flex-none text-yellow-500 text-2xl sm:text-3xl">
+                    <ion-icon name="heart"></ion-icon>
+                </div>
                 <h1 class="pb-6 sm:text-lg">Propósitos</h1>
-                <div class="font-light text-sm text-center leading-6 sm:text-base">Texto a definir</div>
+                <div class="font-light text-sm text-center sm:text-base">
+                    O propósito maior da Igreja MSV é fazer com que o evangelho do Senhor Jesus Cristo seja proclamado,
+                    anunciado, publicado, declarado e propagado em toda sua magnificente plenitude, afim, de que as
+                    vidas que lhe deem ouvidos sejam abençoadas e salvas.
+                </div>
             </div>
         </div>
     </div>
