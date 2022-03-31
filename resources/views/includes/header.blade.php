9b3eb37341294edc1ccdbc4b01fdb141642f5b24
Jefferson Silva <jeffersonassilva@gmail.com>
2022-03-31 19:18:57 -0300

    feat: trocando o banner e ajustando os menus

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,17 +1,17 @@
-<header class="flex justify-between items-center p-4 lg:p-6 xl:p-8">
+<header class="flex justify-between items-center p-4 lg:p-6">
     <a href="{{ route('index') }}">
         <img src="{{ asset('/img/logo-branca.png') }}" alt="logo" class="w-[70px] sm:w-[90px] md:w-[110px] md:w-[130px]">
     </a>
     <div class="toggle lg:hidden"></div>
     <ul class="navigation hidden lg:flex">
-        <li class="link"><a href="{{ route('index') }}">Home</a></li>
-        <li class="link"><a href="/#propositos">Propósitos</a></li>
-        <li class="link"><a href="{{ route('ofertas') }}">Ofertas</a></li>
-        <li class="link"><a href="https://horadeberear.com.br/" target="_blank">Blog</a></li>
-        <li class="link"><a href="https://www.flickr.com/photos/igrejamsv/albums" target="_blank">Fotos</a></li>
-        <li class="link"><a href="/#pastores">Pastores</a></li>
-        <li class="link"><a href="/#programacao">Programação</a></li>
-        <li class="link"><a href="/#contato">Contato</a></li>
+        <li class="text-white"><a href="{{ route('index') }}">Home</a></li>
+        <li class="text-white"><a href="/#propositos">Propósitos</a></li>
+        <li class="text-white"><a href="{{ route('ofertas') }}">Ofertas</a></li>
+        <li class="text-white"><a href="https://horadeberear.com.br/" target="_blank">Blog</a></li>
+        <li class="text-white"><a href="https://www.flickr.com/photos/igrejamsv/albums" target="_blank">Fotos</a></li>
+        <li class="text-white"><a href="/#pastores">Pastores</a></li>
+        <li class="text-white"><a href="/#programacao">Programação</a></li>
+        <li class="text-white"><a href="/#contato">Contato</a></li>
         {{-- <li><a href="{{ route('login') }}">Login</a></li>--}}
     </ul>
 </header>
