6318df3a030fa8e0752de5111c22c4500c523619
Jefferson Silva <jeffersonassilva@gmail.com>
2022-04-18 20:04:05 -0300

    feat: ajustes do layout das fotos - pastores e video

--- resources/views/includes/pastores.blade.php
+++ resources/views/includes/pastores.blade.php
@@ -2,14 +2,17 @@
     <div class="container mx-auto max-w-[1280px]">
         <h1 class="titulo-separador">Pastores</h1>
         <div class="flex flex-col flex-wrap md:flex-row p-3 lg:py-10 lg:px-20">
-            <div class="md:flex-[50%] md:flex-1 flex md:justify-end md:mx-3 bg-gray-200">
+            <div class="md:flex-[50%] md:flex-1 flex md:justify-end md:mx-3 bg-gray-200 rounded-md">
                 <picture class="w-full">
                     <source type="image/webp" media="(min-width:640px)" srcset="{{ asset('img/pastores-large.webp') }}">
                     <source type="image/jpeg" media="(min-width:640px)" srcset="{{ asset('img/pastores-large.jpg') }}">
                     <source type="image/webp" media="(min-width:480px)" srcset="{{ asset('img/pastores-medium.webp') }}">
                     <source type="image/jpeg" media="(min-width:480px)" srcset="{{ asset('img/pastores-medium.jpg') }}">
                     <source type="image/webp" media="(max-width:479px)" srcset="{{ asset('img/pastores-small.webp') }}">
-                    <img loading="lazy" width="720" height="480" src="{{ asset('img/pastores-small.jpg') }}" alt="Samuel e Hacsa" title="Samuel e Hacsa">
+                    <img loading="lazy" width="720" height="480" src="{{ asset('img/pastores-small.jpg') }}"
+                         alt="Samuel e Hacsa" title="Samuel e Hacsa"
+                         class="rounded-md"
+                    >
                 </picture>
             </div>
             <div class="md:flex-[50%] md:flex-1">
