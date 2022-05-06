9ac54727d5b3f5ba265e071a3034514d41608e24
Jefferson Silva <jeffersonassilva@gmail.com>
2022-05-06 17:19:16 -0300

    feat: funcionalidades de adicionar e arquivar banners

--- app/Http/Controllers/Admin/BannerController.php
+++ app/Http/Controllers/Admin/BannerController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\BannerRequest;
 use App\Services\BannerService;
-use Illuminate\Support\Facades\Storage;
 
 class BannerController extends Controller
 {
@@ -13,11 +12,6 @@ class BannerController extends Controller
      * @var BannerService
      */
     private $service;
-
-    /**
-     * @var string
-     */
-    private $diretorio;
 
     /**
      * BannerController constructor.
@@ -30,13 +24,30 @@ class BannerController extends Controller
     }
 
     /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function create()
+    {
+        return view('admin/banners/create');
+    }
+
+    /**
+     * @param BannerRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(BannerRequest $request)
+    {
+        $this->service->store($request);
+        return redirect()->route('dashboard');
+    }
+
+    /**
      * @param $id
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function edit($id)
     {
         $data = $this->service->edit($id);
-
         return view('admin/banners/edit')->with(['data' => $data]);
     }
 
@@ -47,126 +58,17 @@ class BannerController extends Controller
      */
     public function update(BannerRequest $request, $id)
     {
-        $dataRequest = $request->all();
-        $urlImagens = $this->uploadImagens($request);
-        $dados = array_merge($dataRequest, $urlImagens);
-
-        $data = $this->service->edit($id);
-        $this->removerImagensAntigas($data);
-
-        $data->fill($dados)->save();
-
+        $this->service->update($request, $id);
         return redirect()->route('dashboard');
     }
 
     /**
-     * @param $request
-     * @return array
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
      */
-    private function uploadImagens($request)
+    public function destroy($id)
     {
-        $time = date('YmdHis');
-        $this->uploadImgOriginal($time, $request);
-
-        return array(
-            'img_mobile' => $this->diretorio . $this->uploadImgMobile($time, $request),
-            'img_web' => $this->diretorio . $this->uploadImgWeb($time, $request)
-        );
-    }
-
-    /**
-     * @param $time
-     * @param $request
-     * @return void
-     */
-    private function uploadImgOriginal($time, $request)
-    {
-        $nomeImagem = $time . '_m.' . $request->file('img_mobile')->extension();
-        Storage::disk('banners')->putFileAs('original', $request->file('img_mobile'), $nomeImagem);
-
-        $nomeImagem = $time . '_w.' . $request->file('img_web')->extension();
-        Storage::disk('banners')->putFileAs('original', $request->file('img_web'), $nomeImagem);
-    }
-
-    /**
-     * @param $time
-     * @param $request
-     * @return false|string
-     */
-    private function uploadImgMobile($time, $request)
-    {
-        $extension = $request->file('img_mobile')->extension();
-        $nomeImagem = $time . '.jpg';
-
-        $name = Storage::disk('banners')->putFileAs('mobile', $request->file('img_mobile'), $nomeImagem);
-
-        if (Storage::disk('banners')->exists($name)) {
-            $this->optimizarImagem($this->diretorio . $name, $extension, 640, 360);
-        }
-
-        return $name;
-    }
-
-    /**
-     * @param $time
-     * @param $request
-     * @return string
-     */
-    private function uploadImgWeb($time, $request)
-    {
-        $extension = $request->file('img_web')->extension();
-        $nomeImagem = $time . '.jpg';
-
-        $name = Storage::disk('banners')->putFileAs('web', $request->file('img_web'), $nomeImagem);
-
-        if (Storage::disk('banners')->exists($name)) {
-            $this->optimizarImagem($this->diretorio . $name, $extension, 1920, 400);
-        }
-
-        return $name;
-    }
-
-    /**
-     * @param $fileName
-     * @param $extension
-     * @param $width
-     * @param $height
-     * @return void
-     */
-    private function optimizarImagem($fileName, $extension, $width, $height)
-    {
-        if ($extension == 'jpg' || $extension == 'jpeg') {
-            $image = @imagecreatefromjpeg($fileName);
-            $img = imagescale($image, $width, $height);
-            imagejpeg($img, $fileName);
-
-        } elseif ($extension == 'png') {
-            $image = @imagecreatefrompng($fileName);
-            $bg = imagecreatetruecolor(imagesx($image), imagesy($image));
-            imagefill($bg, 0, 0, imagecolorallocate($bg, 255, 255, 255));
-            imagealphablending($bg, true);
-            imagecopy($bg, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
-
-            $img = imagescale($bg, $width, $height);
-
-            imagedestroy($bg);
-            imagejpeg($img, $fileName);
-
-        } else {
-            return;
-        }
-
-        imagedestroy($image);
-        imagedestroy($img);
-    }
-
-    /**
-     * @param $data
-     * @return void
-     */
-    private function removerImagensAntigas($data)
-    {
-        Storage::disk('banners')->delete(str_replace($this->diretorio, '', $data->img_mobile));
-        Storage::disk('banners')->delete(str_replace($this->diretorio, '', $data->img_web));
+        $this->service->destroy($id);
+        return redirect()->route('dashboard');
     }
 }
---
+++ resources/views/admin/banners/create.blade.php
@@ -0,0 +1,67 @@
+<x-app-layout>
+    <h2 class="font-thin text-2xl text-gray-500 p-3">
+        Adicionar Novo Banner
+    </h2>
+
+    <div class="p-3">
+        <div class="">
+            <form class="form-horizontal" role="form"
+                  action="{{ route('banners.store') }}"
+                  method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="flex flex-col mb-6 p-6 bg-white">
+                    <label for="descricao" class="text-gray-900 mb-2">Descrição</label>
+                    <span class="text-sm font-thin text-gray-400 mb-2">- Máximo de 100 caracteres</span>
+                    <input type="text" name="descricao" id="descricao" maxlength="100"
+                           class="border-gray-400 rounded-sm text-gray-700 @error('descricao') border-[1px] border-red-500 @enderror"
+                           value="{{ old('descricao') }}">
+                    @error('descricao')
+                    <span class="text-red-500 text-xs mt-1">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col mb-6 p-6 bg-white">
+                    <label for="img_mobile" class="text-gray-900 mb-2">Imagem Mobile</label>
+                    <span class="text-sm font-thin text-gray-500">- A imagem deve respeitar a proporção 16/9.</span>
+                    <span class="text-sm font-thin text-gray-500">- O tamanho mínimo é de 640x360.</span>
+                    <span class="text-sm font-thin text-gray-500 mb-2"> - Extensões válidas: jpg, jpeg e png.</span>
+                    <input type="file" name="img_mobile" id="img_mobile"
+                           class="@error('img_mobile') border-[1px] border-red-500 @enderror"
+                           value="{{ old('img_mobile') }}">
+                    @error('img_mobile')
+                    <span class="text-red-500 text-xs mt-1">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col mb-6 p-6 bg-white">
+                    <label for="img_web" class="text-gray-900 mb-2">Imagem Web</label>
+                    <span class="text-sm font-thin text-gray-500"> - A imagem deve ter o tamanho de 1920x400.</span>
+                    <span class="text-sm font-thin text-gray-500 mb-2"> - Extensões válidas: jpg, jpeg e png.</span>
+                    <input type="file" name="img_web" id="img_web"
+                           class="@error('img_web') border-[1px] border-red-500 @enderror"
+                           value="{{ old('img_web') }}">
+                    @error('img_web')
+                    <span class="text-red-500 text-xs mt-1">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <button aria-label="Salvar" type="submit"
+                       class="outline-0 rounded-md text-blue-400 border border-blue-400
+                                            hover:text-white hover:bg-blue-400
+                                            focus:text-white focus:bg-blue-400
+                                            px-4 py-1 inline-flex justify-center items-center">
+                        <span class="ml-1">Salvar</span>
+                    </button>
+                    <a href="{{ route('dashboard') }}" aria-label="Voltar" type="button"
+                            class="outline-0 rounded-md text-blue-400 border border-blue-400
+                                            hover:text-white hover:bg-blue-400
+                                            focus:text-white focus:bg-blue-400
+                                            px-4 py-1 ml-2 inline-flex justify-center items-center">
+                        <span class="ml-1">Voltar</span>
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
+</x-app-layout>
