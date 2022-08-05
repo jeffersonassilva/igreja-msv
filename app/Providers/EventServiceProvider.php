8cfb3dbe3e03f77a19af8041fa9bfb0f92b5fea0
Jefferson Silva <jeffersonassilva@gmail.com>
2022-08-05 11:46:29 -0300

    feat: adicionando o observe de propositos - controle de cache

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Banner;
+use App\Models\Proposito;
 use App\Observers\BannerObserver;
+use App\Observers\PropositoObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -30,5 +32,6 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         Banner::observe(BannerObserver::class);
+        Proposito::observe(PropositoObserver::class);
     }
 }
