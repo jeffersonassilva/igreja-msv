177d31e3582039f53c4041a594126c55d2b4240e
Jefferson Silva <jeffersonassilva@gmail.com>
2022-08-19 16:05:23 -0300

    feat: alterando os controladores de escalas e voluntários

--- app/Http/Controllers/VoluntarioController.php
+++ app/Http/Controllers/VoluntarioController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Helpers\Constants;
 use App\Services\VoluntarioService;
-use Illuminate\Http\Request;
 
 /**
  * Class VoluntarioController
@@ -23,40 +21,5 @@ class VoluntarioController extends Controller
     public function __construct(VoluntarioService $service)
     {
         $this->service = $service;
-    }
-
-    /**
-     * @param Request $request
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function store(Request $request)
-    {
-        $this->service->store($request);
-        return redirect('escalas/#' . $request->get('escala_id'));
-    }
-
-    /**
-     * @param Request $request
-     * @param $id
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function update(Request $request, $id)
-    {
-        $voluntario = $this->service->update($request, $id);
-        return redirect()->route('escalas.edit', $voluntario->escala_id)->with(
-            Constants::MESSAGE, __(Constants::SUCCESS_UPDATE)
-        );
-    }
-
-    /**
-     * @param $id
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function destroy($id)
-    {
-        $voluntario = $this->service->destroy($id);
-        return redirect()->route('escalas.edit', $voluntario->escala_id)->with(
-            Constants::MESSAGE, __(Constants::SUCCESS_DESTROY)
-        );
     }
 }
