f3e8a1350ccea6efa42b80019ec85aa12c7305e1
Jefferson Silva <jeffersonassilva@gmail.com>
2022-10-06 23:16:19 -0300

    feat: correção de permissões dos botões

--- resources/views/admin/usuarios/index.blade.php
+++ resources/views/admin/usuarios/index.blade.php
@@ -28,31 +28,33 @@
                         {{ $usuario->email }}
                     </p>
                     @if($usuario->email !== 'jeffersonassilva@gmail.com')
-                    <div class="text-rights text-sm mt-3">
-                        @can('adm-editar-usuario')
-                        <a aria-label="Editar" href="{{ route('usuarios.edit', $usuario) }}"
-                           class="outline-0 rounded-md text-blue-400 border border-blue-400
-                                hover:text-white hover:bg-blue-400
-                                focus:text-white focus:bg-blue-400
-                                px-2 py-1 inline-flex justify-center items-center">
-                            <ion-icon name="create-outline"></ion-icon><span class="ml-1">Editar</span>
-                        </a>
-                        @endcan
+                        @canany(['adm-editar-usuario', 'adm-excluir-usuario'])
+                        <div class="text-rights text-sm mt-3">
+                            @can('adm-editar-usuario')
+                            <a aria-label="Editar" href="{{ route('usuarios.edit', $usuario) }}"
+                               class="outline-0 rounded-md text-blue-400 border border-blue-400
+                                    hover:text-white hover:bg-blue-400
+                                    focus:text-white focus:bg-blue-400
+                                    px-2 py-1 inline-flex justify-center items-center">
+                                <ion-icon name="create-outline"></ion-icon><span class="ml-1">Editar</span>
+                            </a>
+                            @endcan
 
-                        @can('adm-excluir-usuario')
-                        <form action="{{ route('usuarios.destroy', $usuario) }}" method="POST" class="inline">
-                            @method('DELETE')
-                            @csrf
-                            <button aria-label="Excluir"
-                                    class="outline-0 rounded-md text-blue-400 border border-blue-400
-                                hover:text-white hover:bg-blue-400
-                                focus:text-white focus:bg-blue-400
-                                px-2 py-1 mr-1 inline-flex justify-center items-center">
-                                <ion-icon name="trash-outline"></ion-icon><span class="ml-1">Excluir</span>
-                            </button>
-                        </form>
-                        @endcan
-                    </div>
+                            @can('adm-excluir-usuario')
+                            <form action="{{ route('usuarios.destroy', $usuario) }}" method="POST" class="inline">
+                                @method('DELETE')
+                                @csrf
+                                <button aria-label="Excluir"
+                                        class="outline-0 rounded-md text-blue-400 border border-blue-400
+                                    hover:text-white hover:bg-blue-400
+                                    focus:text-white focus:bg-blue-400
+                                    px-2 py-1 mr-1 inline-flex justify-center items-center">
+                                    <ion-icon name="trash-outline"></ion-icon><span class="ml-1">Excluir</span>
+                                </button>
+                            </form>
+                            @endcan
+                        </div>
+                        @endcanany
                     @endif
                 </div>
             @endforeach
