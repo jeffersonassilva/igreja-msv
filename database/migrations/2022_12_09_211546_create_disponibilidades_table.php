4b911bdeeff77575f967cef78f99bbf1f325b6b2
Jefferson Silva <jeffersonassilva@gmail.com>
2022-12-10 16:05:48 -0300

    feat: migration da tabela de disponibilidades dos voluntários

---
+++ database/migrations/2022_12_09_211546_create_disponibilidades_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDisponibilidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('disponibilidades', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('voluntario_id')
+                ->comment('Id do voluntário')
+                ->constrained('voluntarios')
+                ->cascadeOnDelete();
+
+            $table->integer('dia')
+                ->comment('Dia da semana representado por números. Ex: 1=Domingo');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('disponibilidades');
+    }
+}
