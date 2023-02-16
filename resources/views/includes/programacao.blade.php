7becaadc88a7a36db4188b36e6b6403045022553
Jefferson Silva <jeffersonassilva@gmail.com>
2023-02-15 23:43:31 -0300

    feat: adicionando o culto de homens na programação

--- resources/views/includes/programacao.blade.php
+++ resources/views/includes/programacao.blade.php
@@ -1,15 +1,18 @@
 <section id="programacao" class="flex flex-col items-center p-3 pb-10 md:p-0 md:py-10 md:pb-20">
-    <div class="container mx-auto max-w-[1280px]">
+    <div class="container md:mx-auto md:max-w-[1280px]">
         <h1 class="titulo-separador">Programação</h1>
-        <div class="flex flex-col flex-wrap p-3 w-full md:flex-row md:px-12">
+        <div class="grid md:grid-cols-2 xl:grid-cols-3 p-3 w-full md:flex-row md:px-12">
             <div class="flex gap-4 py-4 md:basis-1/2 lg:basis-1/3">
                 <div class="text-2xl pt-1">
                     <ion-icon name="calendar-outline"></ion-icon>
                 </div>
                 <div class="flex flex-col">
-                    <div class="font-medium mb-1 md:text-xl">Terça-feira</div>
-                    <div class="text-sm text-gray-500 md:text-base">
-                        <span class="font-semibold">20:00h</span> - Reunião de Mulheres
+                    <div class="font-medium mb-1 lg:text-xl">Terça-feira</div>
+                    <div class="text-sm text-gray-500 lg:text-base">
+                        <span class="font-semibold">20:00h</span> - Reunião de Mulheres (quinzenal)
+                    </div>
+                    <div class="text-sm text-gray-500 lg:text-base">
+                        <span class="font-semibold">20:00h</span> - Reunião de Homens (quinzenal)
                     </div>
                 </div>
             </div>
@@ -18,8 +21,8 @@
                     <ion-icon name="calendar-outline"></ion-icon>
                 </div>
                 <div class="flex flex-col">
-                    <div class="font-medium mb-1 md:text-xl">Quarta-feira</div>
-                    <div class="text-sm text-gray-500 md:text-base">
+                    <div class="font-medium mb-1 lg:text-xl">Quarta-feira</div>
+                    <div class="text-sm text-gray-500 lg:text-base">
                         <span class="font-semibold">16:00h</span> - Culto de Oração
                     </div>
                 </div>
@@ -29,8 +32,8 @@
                     <ion-icon name="calendar-outline"></ion-icon>
                 </div>
                 <div class="flex flex-col">
-                    <div class="font-medium mb-1 md:text-xl">Quinta-feira</div>
-                    <div class="text-sm text-gray-500 md:text-base">
+                    <div class="font-medium mb-1 lg:text-xl">Quinta-feira</div>
+                    <div class="text-sm text-gray-500 lg:text-base">
                         <span class="font-semibold">20:00h</span> - Culto de Imersão
                     </div>
                 </div>
@@ -40,8 +43,8 @@
                     <ion-icon name="calendar-outline"></ion-icon>
                 </div>
                 <div class="flex flex-col">
-                    <div class="font-medium mb-1 md:text-xl">Sábado</div>
-                    <div class="text-sm text-gray-500 md:text-base">
+                    <div class="font-medium mb-1 lg:text-xl">Sábado</div>
+                    <div class="text-sm text-gray-500 lg:text-base">
                         <span class="font-semibold">19:30h</span> - Conexão Jovem
                     </div>
                 </div>
@@ -51,11 +54,11 @@
                     <ion-icon name="calendar-outline"></ion-icon>
                 </div>
                 <div class="flex flex-col">
-                    <div class="font-medium mb-1 md:text-xl">Domingo</div>
-                    <div class="text-sm text-gray-500 md:text-base">
+                    <div class="font-medium mb-1 lg:text-xl">Domingo</div>
+                    <div class="text-sm text-gray-500 lg:text-base">
                         <span class="font-semibold">09:00h</span> - Escola Bíblica Dominical
                     </div>
-                    <div class="text-sm text-gray-500 md:text-base">
+                    <div class="text-sm text-gray-500 lg:text-base">
                         <span class="font-semibold">19:00h</span> - Culto Público
                     </div>
                 </div>
