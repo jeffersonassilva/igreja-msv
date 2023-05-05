20ba2d6b98ceafc7ff806c83de8712519ea6dbaa
Jefferson Silva <jeffersonassilva@gmail.com>
2023-05-05 16:29:35 -0300

    feat: criando o grupo de menus de gestão de escalas

--- resources/views/components/adm-menu-item.blade.php
+++ resources/views/components/adm-menu-item.blade.php
@@ -1,8 +1,8 @@
 @props(['route', 'group', 'icon'])
 
 <a href="@if($route === '#') # @else {{ route($route) }} @endif" class="@if(preg_match('/^admin\/' . $group . '($|\/|\?)/', request()->path())) active @endif
-    border-t border-t-gray-100 px-4 lg:w-full h-14 relative text-gray-500
-    flex gap-4 items-center lg:hover:pl-5
+    border-t border-t-gray-100 px-2 lg:w-full h-14 relative text-gray-500
+    flex gap-4 items-center lg:hover:pl-3
     transition-all duration-300 ease-in-out">
     <div class="lg:w-[22px]">
         <ion-icon name="{{ $icon }}"></ion-icon>
