1fe18b09ae5e06507e5d1176f7415337e5171b08
Jefferson Silva <jeffersonassilva@gmail.com>
2023-12-02 12:10:27 -0300

    feat: inclusão de avatar na escala de limpeza

--- resources/views/components/card/escala-publica.blade.php
+++ resources/views/components/card/escala-publica.blade.php
@@ -31,7 +31,27 @@
             <ul class="text-sm leading-7 font-thin @if($escala->fechada) text-gray-700 @else text-gray-500 @endif">
                 @foreach($escala->voluntarios as $voluntario)
                     <li class="line-clamp-1">
-                        <div class="inline">
+                        <div class="flex items-center">
+                            <div class="mx-1">
+                                @if($voluntario->voluntario->foto)
+                                    <img src="{{ asset($voluntario->voluntario->foto) }}"
+                                         alt="avatar" class="w-[30px] h-[30px] rounded-full
+                                             object-cover aspect-square p-[1px] border-2
+                                             @if($escala->fechada) border-[#cddccd] @else border-gray-100 @endif">
+                                @else
+                                    @if($voluntario->voluntario->sexo == 'M')
+                                        <img src="{{ asset('img/icon_profile_man.jpg') }}"
+                                             alt="avatar" class="w-[30px] h-[30px] rounded-full
+                                                 object-cover aspect-square p-[1px] border-2
+                                                 @if($escala->fechada) border-[#cddccd] @else border-gray-100 @endif">
+                                    @else
+                                        <img src="{{ asset('img/icon_profile_woman.jpg') }}"
+                                             alt="avatar" class="w-[30px] h-[30px] rounded-full
+                                                 object-cover aspect-square p-[1px] border-2
+                                                 @if($escala->fechada) border-[#cddccd] @else border-gray-100 @endif">
+                                    @endif
+                                @endif
+                            </div>
                             {{ $voluntario->voluntario->nome }}
                             @if($voluntario->user_id)
                                 <button type="button" class="ml-1"
