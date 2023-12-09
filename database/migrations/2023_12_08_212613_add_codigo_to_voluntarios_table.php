0687de123dc21e1acc4e06001008e5168978c022
Jefferson Silva <jeffersonassilva@gmail.com>
2023-12-08 21:37:07 -0300

    feat: add campo referente ao código de cada voluntário

---
+++ database/migrations/2023_12_08_212613_add_codigo_to_voluntarios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCodigoToVoluntariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('voluntarios', function (Blueprint $table) {
+            $table->string('codigo', 10)->nullable()->after('situacao');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('voluntarios', function (Blueprint $table) {
+            $table->dropColumn('codigo');
+        });
+    }
+}
