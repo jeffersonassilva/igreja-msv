e58192738f92bb6a6462616d1bda95cd0038f261
Jefferson Silva <jeffersonassilva@gmail.com>
2024-02-08 18:19:57 -0300

    feat: exibição dos erros do formulário

--- resources/views/components/required-fields.blade.php
+++ resources/views/components/required-fields.blade.php
@@ -2,9 +2,13 @@
     @if($errors->any())
         <div id="message_alert" style="background-color: #ffb9b9;"
              class="p-4 mb-4 rounded-md flex justify-between items-center">
-            <span class="text-gray-700 pl-2">
-                Campos obrigatórios não preenchidos!
-            </span>
+            <div>
+                @foreach ($errors->all() as $erro)
+                    <div class="text-gray-700 pl-2">
+                        {{ $erro }}
+                    </div>
+                @endforeach
+            </div>
             <span class="text-2xl flex items-center cursor-pointer" id="close-message-btn">
                 <ion-icon name="close-outline"></ion-icon>
             </span>
