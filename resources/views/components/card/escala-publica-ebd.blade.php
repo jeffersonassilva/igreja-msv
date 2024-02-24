c647b6b045dd138d31c54b7552f30959c251a805
Jefferson Silva <jeffersonassilva@gmail.com>
2024-02-24 17:07:03 -0300

    feat: novo breakpoint

--- resources/views/components/card/escala-publica-ebd.blade.php
+++ resources/views/components/card/escala-publica-ebd.blade.php
@@ -4,14 +4,16 @@
     <div class="flex relative border-b border-b-gray-200">
         @if($escala->classe && $escala->classe->revista)
             <img src="{{ asset($escala->classe->revista) }}" alt="capa da revista"
-                 class="w-[90px] h-[130px] mt-4 cursor-pointer
+                 class="w-[62px] h-[90px] mt-4 cursor-pointer
+                        xs:w-[90px] xs:h-[130px] xs:mt-4
                         sm:w-[120px] sm:h-[175px] sm:mt-[-10px]
                         md:w-[145px] md:h-[210px] md:mt-[-20px] md:mb-4 rounded-sm md:object-cover
                         transition-all ease-in-out duration-300 hover:scale-105 shadow-md shadow-gray-300">
         @else
             <img src="{{ asset('img/capa-revista-em-branco.jpg') }}" alt="capa da revista"
-                 class="w-[90px] h-[130px] mt-4 cursor-pointer
-                        sm:w-[120px] sm:h-[175px]
+                 class="w-[62px] h-[90px] mt-4 cursor-pointer
+                        xs:w-[90px] xs:h-[130px] xs:mt-4
+                        sm:w-[120px] sm:h-[175px] sm:mt-[-10px]
                         md:w-[145px] md:h-[210px] md:mt-[-20px] md:mb-4 rounded-sm md:object-cover
                         transition-all ease-in-out duration-300 hover:scale-105 shadow-md shadow-gray-300">
         @endif
@@ -23,21 +25,21 @@
                     ({{ \Carbon\Carbon::parse($escala->data)->format('d/m') }})
                 </div>
             </div>
-            <div class="text-gray-500 text-sm font-thin mt-[-5px] sm:mt-[-4px] md:mt-[-2px]">
+            <div class="text-gray-500 text-sm font-thin mt-[-3px] text-ellipsis overflow-hidden line-clamp-1">
                 {{ $escala->classe->faixa_etaria }}&nbsp;
             </div>
 
             <div class="mt-2 sm:mt-3 md:mt-4">
                 <div class="text-sm font-normal">Professor(a):</div>
                 <div class="text-gray-500 text-sm font-thin md:text-ellipsis md:overflow-hidden md:line-clamp-1">
-                    {{ $escala->professor ? $escala->professor->nome : null }}
+                    {{ $escala->professor ? $escala->professor->nome : null }}&nbsp;
                 </div>
             </div>
 
             <div class="mt-2 sm:mt-3 md:mt-4">
                 <div class="text-sm font-normal">Tema:</div>
                 <div class="text-gray-500 text-sm font-thin md:text-ellipsis md:overflow-hidden md:line-clamp-1">
-                    {{ $escala->tema }}
+                    {{ $escala->tema }}&nbsp;
                 </div>
             </div>
         </div>
