36a68f5d555af0f90efc37be4c0687f51d30d606
Jefferson Silva <jeffersonassilva@gmail.com>
2024-02-29 16:09:36 -0300

    feat: ajustes de layout

--- resources/views/includes/pastores.blade.php
+++ resources/views/includes/pastores.blade.php
@@ -2,7 +2,7 @@
     <div class="container mx-auto max-w-[1280px]">
         <h2 class="titulo-separador">Pastores</h2>
         <div class="flex flex-col flex-wrap md:flex-row p-3 lg:py-10 lg:px-20">
-            <div class="md:flex-[50%] md:flex-1 flex md:justify-end md:mx-3 bg-gray-200 rounded-md">
+            <div class="md:flex-1 flex md:justify-end md:mx-3 bg-gray-200 rounded-md">
                 <picture class="w-full">
                     <source type="image/webp" media="(min-width:640px)" srcset="{{ asset('img/pastores-large.webp') }}">
                     <source type="image/jpeg" media="(min-width:640px)" srcset="{{ asset('img/pastores-large.jpg') }}">
@@ -15,11 +15,12 @@
                     >
                 </picture>
             </div>
-            <div class="md:flex-[50%] md:flex-1 flex flex-col justify-center">
+            <div class="md:flex-1 flex flex-col justify-center">
                 <h1 class="text-lg lg:text-2xl font-medium my-6 md:mx-3 lg:my-0 lg:mx-6 2xl:mx-10 text-neutral-700">
                     {{ $pastor->titulo }}
                 </h1>
-                <div class="font-light text-sm lg:text-base 2xl:text-xl leading-6 lg:leading-7 2xl:leading-9 mb-6 md:mx-3 lg:m-6 2xl:m-10">
+                <div class="font-light text-sm lg:text-base 2xl:text-xl leading-6 lg:leading-7 2xl:leading-9 mb-6
+                            md:mx-3 lg:m-6 2xl:m-10">
                     {{ $pastor->descricao }}
                 </div>
             </div>
