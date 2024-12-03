9d4815bd7d6f3f3e2b1372f4ffaf347d11be51e3
Jefferson Silva <jeffersonassilva@gmail.com>
2024-12-03 16:57:03 -0300

    feat: correções de layout

--- resources/views/admin/visitantes/index.blade.php
+++ resources/views/admin/visitantes/index.blade.php
@@ -6,32 +6,46 @@
     <section>
         <div class="h-[60px] bg-white p-3 rounded-md flex items-center justify-center dark:bg-[#252c47]">
             <div class="text-sm">
-                <x-button.link title="Adicionar Visitante" target="_blank" :route="route('visitantes.create')"></x-button.link>
+                <x-button.link
+                    title="Adicionar Visitante"
+                    target="_blank"
+                    :route="route('visitantes.create')">
+                </x-button.link>
             </div>
         </div>
         @if(count($visitantes))
-            <div class="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 md:py-3 gap-4">
+            <div class="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 py-3 gap-4">
                 @foreach($visitantes as $visitante)
-                    <div class="bg-white p-3 shadow-sm rounded-md border-[1px] border-gray-200 dark:bg-[#252c47] dark:border-[#252c47]">
+                    <div class="bg-white p-3 shadow-sm rounded-md border-[1px]
+                                border-gray-200 dark:bg-[#252c47] dark:border-[#252c47]">
                         <div class="grid grid-cols-[68px,1fr] items-center">
                             <div>
                                 @if($visitante->sexo == 'M')
                                     <img src="{{ asset('img/icon_profile_man.jpg') }}"
-                                         alt="avatar" class="w-[60px] rounded-full object-cover aspect-square border-2 border-gray-100 p-[2px] dark:border-[#454b54]">
+                                         alt="avatar"
+                                         class="w-[60px] rounded-full object-cover aspect-square
+                                                border-2 border-gray-100 p-[2px] dark:border-[#454b54]">
                                 @else
                                     <img src="{{ asset('img/icon_profile_woman.jpg') }}"
-                                         alt="avatar" class="w-[60px] rounded-full object-cover aspect-square border-2 border-gray-100 p-[2px] dark:border-[#454b54]">
+                                         alt="avatar"
+                                         class="w-[60px] rounded-full object-cover aspect-square
+                                                border-2 border-gray-100 p-[2px] dark:border-[#454b54]">
                                 @endif
                             </div>
                             <div>
-                                <h3 class="text-gray-700 font-medium dark:text-white line-clamp-1">{{ $visitante->nome }}</h3>
+                                <h3 class="text-gray-700 font-medium dark:text-white line-clamp-1">
+                                    {{ $visitante->nome }}
+                                </h3>
                                 <p class="text-sm mt-1 font-thin text-gray-500 dark:text-[#d0d9e6]">
                                     Visitou em: {{ \Carbon\Carbon::parse($visitante->dt_visita)->format('d/m/Y') }}
                                 </p>
                                 <p class="text-sm mt-1 font-thin text-gray-500 dark:text-[#d0d9e6]">
-                                    Telefone: <a href="https://wa.me/55{{ preg_replace('/\D/', '', $visitante->telefone) }}" target="_blank">{{ $visitante->telefone }}</a>
+                                    Telefone: <a
+                                        href="https://wa.me/55{{ preg_replace('/\D/', '', $visitante->telefone) }}"
+                                        target="_blank">{{ $visitante->telefone }}</a>
                                     @if($visitante->telefone && $visitante->whatsapp)
-                                        <a href="https://wa.me/55{{ preg_replace('/\D/', '', $visitante->telefone) }}" target="_blank">
+                                        <a href="https://wa.me/55{{ preg_replace('/\D/', '', $visitante->telefone) }}"
+                                           target="_blank">
                                             <span class="ml-1 text-green-500">
                                                 <ion-icon name="logo-whatsapp"></ion-icon>
                                             </span>
@@ -46,8 +60,12 @@
                             </div>
                         </div>
                         @can('adm-editar-visitante')
-                            <div class="text-sm flex gap-2 border-t border-t-gray-100 dark:border-t-[#34384e] mt-2 pt-2">
-                                <x-button.link title="Acompanhar" :route="route('visitantes.edit', $visitante)"></x-button.link>
+                            <div class="text-sm flex gap-2
+                                        border-t border-t-gray-100 dark:border-t-[#34384e] mt-2 pt-2">
+                                <x-button.link
+                                    title="Acompanhar"
+                                    :route="route('visitantes.edit', $visitante)">
+                                </x-button.link>
                             </div>
                         @endcan
                     </div>
