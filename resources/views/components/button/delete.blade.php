d233f1e36a051ea318fc2eac568dd8b18f07e287
Jefferson Silva <jeffersonassilva@gmail.com>
2025-01-16 14:03:09 -0300

    feat: modificando o dropdown de ações

--- resources/views/components/button/delete.blade.php
+++ resources/views/components/button/delete.blade.php
@@ -4,15 +4,24 @@
     'formId',
     'bg' => 'text-primary-dark bg-neutral hover:bg-neutral-dark focus:bg-neutral-dark
             dark:text-[#d0d9e6] dark:bg-[#51596b] dark:hover:bg-[#656b7b]',
-    'icon' => null
+    'icon' => null,
+    'dropdown' => false
 ])
+
+@php
+    $class = $attributes->merge(['class' => 'btn-dialog-open outline-0 rounded-md
+            px-3 py-1 mr-1 inline-flex justify-center items-center ' . $bg]);
+
+    if ($dropdown) {
+        $class = $attributes->merge(['class' => 'flex items-center gap-2 w-full text-left px-4 py-2
+            hover:bg-gray-100 dark:hover:bg-gray-700 btn-dialog-open']);
+        }
+@endphp
 
 <form id="submit-{{ $formId }}" action="{{ $route }}" method="POST" class="inline">
     @method('DELETE')
     @csrf
-    <button aria-label="{{ $title }}" type="button" data-form-reference="{{ $formId }}"
-            {{ $attributes->merge(['class' => 'btn-dialog-open outline-0 rounded-md
-            px-3 py-1 mr-1 inline-flex justify-center items-center ' . $bg]) }}>
+    <button aria-label="{{ $title }}" type="button" data-form-reference="{{ $formId }}" {{ $class }}>
         @if($icon)
             <ion-icon name="{{ $icon }}"></ion-icon>
         @endif
