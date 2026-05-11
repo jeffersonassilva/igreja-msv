cc92043d18d7ec76197de1da192b3cafc85d0cdb
Jefferson Silva <jeffersonassilva@gmail.com>
2026-05-11 20:10:47 -0300

    database: add telefone na tabela de voluntários

---
+++ database/migrations/2026_05_11_195000_add_telefone_to_voluntarios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTelefoneToVoluntariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('voluntarios', function (Blueprint $table) {
+            $table->string('telefone', 11)
+                ->nullable()
+                ->after('situacao');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('voluntarios', function (Blueprint $table) {
+            $table->dropColumn('telefone');
+        });
+    }
+}
